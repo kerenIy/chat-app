cf9a7b38a88403b6a6dac9c03912d9766a7ea530
tein <kerp4real@gmail.com>
2024-10-08 17:09:20 +0100

    updated readme, styles

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,28 +4,26 @@ import React from "react";
 export default function Message({ message, currentUser }) {
   return (
     <div>
-      <div className="message">
-        <div
-          className={` ${
-            message.user === currentUser
-              ? "message-content-blue"
-              : "message-content-gray"
-          }`}
-        >
-          <div className="">
-            <div
-              className={`message-bubble ${
-                message.user === currentUser
-                  ? "message-bubble-blue"
-                  : "message-bubble-gray"
-              }`}
-            >
-              <p>{message.message}</p>
-            </div>
-            <div className="message">
-              <span>{message.user}</span>
-              <span className="message-timestamp">{message.timestamp}PM</span>
-            </div>
+      <div
+        className={`message ${
+          message.user === currentUser
+            ? "message-content-blue"
+            : "message-content-gray"
+        }`}
+      >
+        <div>
+          <div
+            className={`message-bubble ${
+              message.user === currentUser
+                ? "message-bubble-blue"
+                : "message-bubble-gray"
+            }`}
+          >
+            <p>{message.message}</p>
+          </div>
+          <div className="message-info">
+            <span>{message.user}</span>
+            <span className="message-timestamp">{message.timestamp} PM</span>
           </div>
         </div>
       </div>
